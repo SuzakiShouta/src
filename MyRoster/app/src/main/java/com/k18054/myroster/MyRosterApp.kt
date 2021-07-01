5d78dbd0c70b97e4d899642c3c93e5c3f2c4fa56
SuzakiShouta <86464601+SuzakiShouta@users.noreply.github.com>
2021-07-01 14:33:33 +0900

    var 1.1.1
    input Type と コメント追加

--- MyRoster/app/src/main/java/com/k18054/myroster/MyRosterApp.kt
+++ MyRoster/app/src/main/java/com/k18054/myroster/MyRosterApp.kt
@@ -7,7 +7,9 @@ import io.realm.RealmConfiguration
 class MyRosterApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        //Realm 初期化
         Realm.init(this)
+        //扱うデータが少量なのでUiThreadをtrue(UIスレッドでも書き込める)に。
         val config = RealmConfiguration.Builder().allowWritesOnUiThread(true).build()
         Realm.setDefaultConfiguration(config)
     }
